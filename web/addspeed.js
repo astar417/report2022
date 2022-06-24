32714780afb9af6e7cf5954f08aa5fda2b180558
astar417 <77217695+astar417@users.noreply.github.com>
2022-06-24 14:56:33 +0800

    20220624

--- web/addspeed.js
+++ web/addspeed.js
@@ -1,10 +1,5 @@
 // JavaScript Document
-{
-  "startPage": 1, // 分片的开始页码
-  "endPage": 2, // 分片结束页码
-  "totalPage": 64, // pdf 总页数
-  "url": "report2022A3.pdf" // 分片内容下载地址
-}
+
 
 /*
   代码中使用 loadStatus 来记录特定页的内容是否一件下载
@@ -39,14 +34,14 @@ asycn function fetchPdfFragment(pageIndex) {
   /* 
     省略具体实现
     该方法从服务器获取包含指定页码(pageIndex)的 pdf 分片内容，
-    返回的格式参考上文约定：
-    {
-      "startPage": 1, // 分片的开始页码
-      "endPage": 5, // 分片结束页码
-      "totalPage": 100, // pdf 总页数
-      "url": "http://test.com/asset/fhdf82372837283.pdf" // 分片内容下载地址
-    }
-  */ 
+    返回的格式参考上文约定： */
+{
+  "startPage": 1, // 分片的开始页码
+  "endPage": 2, // 分片结束页码
+  "totalPage": 64, // pdf 总页数
+  "url": "report2022A3.pdf" // 分片内容下载地址
+}
+  
 }
 // 创建一个 pages 数组来保存已经下载的 pdf 
 function initPages (totalPage) {
@@ -125,7 +120,7 @@ scrollPdf = _.debounce(() => {
   loadAfter(pageIndex);
 }, 200)
 // 假定每个分片的大小是 5 页
-const SLICE_COUNT = 5;
+const SLICE_COUNT = 2;
 // 获取当前页之前页面的分片
 function loadBefore (pageIndex) {
   const start = (Math.floor(pageIndex / SLICE_COUNT) * SLICE_COUNT) - (SLICE_COUNT - 1);
